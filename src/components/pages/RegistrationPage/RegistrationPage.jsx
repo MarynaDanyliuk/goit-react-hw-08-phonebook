5cf1db4f0c5d8f4f4b0567fb7cd8e6d414bc908a
Mary Danyliuk <marydanyliuk@gmail.com>
2023-03-07 17:58:05 +0200

    RegistrationPage in progress

---
+++ src/components/pages/RegistrationPage/RegistrationPage.jsx
@@ -0,0 +1,13 @@
+import css from './RegistrationPage/RegistrationPage.module.css';
+
+// import Movies from 'components/Movies/Movies';
+
+const RegistrationPage = () => {
+  return (
+    <div className={css.container}>
+      <h2 className={css.page_title}>Registration page</h2>
+      {/* <Movies /> */}
+    </div>
+  );
+};
+export default RegistrationPage;
